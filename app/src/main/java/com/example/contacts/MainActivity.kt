223008f2dedb4e3b7ec4c315ccfca1582fe7364b
AndrewLukiv <andrew.lukiv@gmail.com>
2023-05-30 06:10:02 +0200

    Fixed a bug in CursorIterator. Instead of the first row, 'CursorIterator' were begun iterating from the second. The navigation host was created with routes for contacts and raw contacts of specific contacts. The retrieval and display of raw contacts(account type and name) for the contact lookup key were implemented.

--- app/src/main/java/com/example/contacts/MainActivity.kt
+++ app/src/main/java/com/example/contacts/MainActivity.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import android.provider.ContactsContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,12 +20,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.contacts.ui.theme.SystemContactsExplorationTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.database.getStringOrNull
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +36,19 @@ class MainActivity : ComponentActivity() {
             SystemContactsExplorationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    ContactsList()
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "contacts") {
+                        composable("contacts") {
+                            ContactsList(onNavigateToRawContacts = { contactId ->
+                                navController.navigate("$contactId/raw_contacts")
+                            })
+                        }
+                        composable("{contact_id}/raw_contacts") { backStackEntry ->
+                            RawContactsList(contactId = backStackEntry.arguments?.getString("contact_id")!!)
+                        }
+                    }
                 }
             }
         }
@@ -42,38 +56,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SystemContactsExplorationTheme {
-        Greeting("Android")
+fun RawContactsList(contactId: String) {
+    val contentResolver = LocalContext.current.contentResolver
+    var rawContacts by remember {
+        mutableStateOf(listOf<RawContact>())
+    }
+    LaunchedEffect(true) {
+        retrieveRawContacts(contactId, contentResolver)?.also { rawContacts = it }
+    }
+    LazyColumn {
+        for (rawContact in rawContacts) {
+            item(rawContact.id) {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(text = rawContact.accountType, style = MaterialTheme.typography.titleLarge)
+                    Text(text = rawContact.accountName, style = MaterialTheme.typography.bodyLarge)
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun ContactsList() {
-    val contentResolver= LocalContext.current.contentResolver
+fun ContactsList(onNavigateToRawContacts: (String) -> Unit) {
+    val contentResolver = LocalContext.current.contentResolver
     var contacts by remember {
-      mutableStateOf(listOf<Contact>())
+        mutableStateOf(listOf<Contact>())
     }
-    LaunchedEffect( true) {
+    LaunchedEffect(true) {
         retrieveContacts(contentResolver)?.also { contacts = it }
     }
-    LazyColumn{
-      for (contact in contacts){
-         item (contact.id){
-             Text(text = contact.name)
-         }
-      }
+    LazyColumn {
+        for (contact in contacts) {
+            item(contact.id) {
+                Text(text = contact.name,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.clickable { onNavigateToRawContacts(contact.id) })
+            }
+        }
     }
 }
+
 data class Contact(
     val id: String,
     val name: String,
@@ -81,32 +103,84 @@ data class Contact(
 
 fun retrieveContacts(contentResolver: ContentResolver): List<Contact>? {
     val cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null)
-    return cursor?.toIterable{
-                val nameColumnIndex =
-                    cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)
-                val idColumnIndex =
-                    cursor.getColumnIndexOrThrow(ContactsContract.Contacts.LOOKUP_KEY)
-                val name = cursor.getStringOrNull(nameColumnIndex).toString()
-                val id = cursor.getString(idColumnIndex)
-                Contact(id, name)
+    val contacts = cursor?.toIterable {
+        val nameColumnIndex =
+            it.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)
+        val idColumnIndex = it.getColumnIndexOrThrow(ContactsContract.Contacts.LOOKUP_KEY)
+        Contact(
+            id = it.getString(idColumnIndex), name = it.getStringOrNull(nameColumnIndex).toString()
+        )
     }?.toList()
+    cursor?.close()
+    return contacts
 }
-class CursorIterator<T>(private val cursor:Cursor,private val converter:(Cursor)->T):Iterator<T>{
+
+data class RawContact(
+    val id: Long, val accountName: String, val accountType: String,
+)
+
+fun retrieveRawContacts(
+    contactLookupId: String, contentResolver: ContentResolver
+): List<RawContact>? {
+    val contactCursor = contentResolver.query(
+        ContactsContract.Contacts.CONTENT_LOOKUP_URI.buildUpon().appendPath(contactLookupId)
+            .build(), arrayOf(ContactsContract.Contacts._ID), null, null
+    )
+    val contactId = contactCursor?.toIterable {
+        val idColumnIndex = it.getColumnIndexOrThrow(ContactsContract.Contacts._ID)
+        it.getLong(idColumnIndex)
+    }?.firstOrNull()
+    contactCursor?.close()
+    return contactId?.let { id ->
+        val cursor = contentResolver.query(
+            ContactsContract.RawContacts.CONTENT_URI,
+            null,
+            "${ContactsContract.RawContacts.CONTACT_ID}=?",
+            arrayOf(id.toString()),
+            null
+        )
+        val rawContacts = cursor?.toIterable {
+            val accountTypeColumnIndex =
+                it.getColumnIndexOrThrow(ContactsContract.RawContacts.ACCOUNT_TYPE)
+            val accountNameColumnIndex =
+                it.getColumnIndexOrThrow(ContactsContract.RawContacts.ACCOUNT_NAME)
+            val idColumnIndex =
+                it.getColumnIndexOrThrow(ContactsContract.RawContacts._ID)
+            RawContact(
+                id = it.getLong(idColumnIndex),
+                accountName = it.getString(accountNameColumnIndex),
+                accountType = it.getString(accountTypeColumnIndex)
+            )
+        }?.toList()
+
+        cursor?.close()
+        rawContacts
+    }
+}
+
+class CursorIterator<T>(private val cursor: Cursor, private val converter: (Cursor) -> T) :
+    Iterator<T> {
+    private var isFirst = true
     override fun hasNext(): Boolean {
         return !cursor.isLast
     }
 
     override fun next(): T {
-        cursor.moveToNext()
+        if (isFirst) {
+            cursor.moveToFirst()
+            isFirst = false
+        } else {
+            cursor.moveToNext()
+        }
         return converter(cursor)
     }
 
 }
-fun <T>Cursor.toIterable(converter: (Cursor) -> T):Iterable<T>{
-    return object :Iterable<T>{
+
+fun <T> Cursor.toIterable(converter: (Cursor) -> T): Iterable<T> {
+    return object : Iterable<T> {
         override fun iterator(): Iterator<T> {
-            this@toIterable.moveToFirst()
-            return CursorIterator(this@toIterable,converter)
+            return CursorIterator(this@toIterable, converter)
         }
     }
 }
